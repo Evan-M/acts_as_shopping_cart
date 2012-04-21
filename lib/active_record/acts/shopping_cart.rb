ddd7f9f8757d199e5bd5a1e0bca4713d4a7ce5b6
Evan Marell <evan@mefunction.com>
2012-04-20 21:52:18 -0700

    Tweaks to make work with polymorphic has_many :through...

--- lib/active_record/acts/shopping_cart.rb
+++ lib/active_record/acts/shopping_cart.rb
@@ -1,8 +1,6 @@
 module ActiveRecord
   module Acts
     module ShoppingCart
-      attr_reader :shopping_cart_items_collection
-      
       def self.included(base)
         base.extend(ClassMethods)
       end
@@ -21,8 +19,8 @@ module ActiveRecord
         def acts_as_shopping_cart_using(item_class)
           self.send :include, ActiveRecord::Acts::ShoppingCart::Cart::InstanceMethods
           self.send :include, ActiveRecord::Acts::ShoppingCart::Item::InstanceMethods
-          @shopping_cart_items_collection = item_class.to_s.pluralize
-          has_many @shopping_cart_items_collection.to_sym, :as => :owner, :dependent => :destroy
+          self.shopping_cart_items_collection = item_class.to_s.pluralize
+          has_many self.shopping_cart_items_collection.to_sym, :as => :owner, :dependent => :destroy
         end
 
         #
@@ -32,6 +30,14 @@ module ActiveRecord
         #
         def acts_as_shopping_cart
           acts_as_shopping_cart_using :shopping_cart_item
+        end
+        
+        def shopping_cart_items_collection
+          @@shopping_cart_items_collection
+        end
+        
+        def shopping_cart_items_collection=(collection_name)
+          @@shopping_cart_items_collection = collection_name
         end
       end
     end
